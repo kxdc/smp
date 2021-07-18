2559d63d540c7e2a920c844794d5928c35a9b05d
kxdc <kvuexlu@gmail.com>
2021-07-18 12:54:31 +0800

    Set python logging in main prototype

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import os
+import logging
 
 def exists_file(filename):
 
     if not os.path.exists(filename):
-        print(filename, " is empty")
+        logger.critical('No such file "%s"', filename)
         return False
     else:
-        print("processing ", filename)
+        logger.info('Processing file "%s"', filename)
         return True
 
 
 def process_file(filename):
 
     if exists_file(filename):
-        # do somting
+        logger.info('Do somthing with "%s"', filename)
     else:
         return
 
+
 if __name__ == '__main__':
 
-    file_path = "/path/to/xxxx"
+    file_path = "./sample.txt"
+    logFormatter = '%(asctime)s - %(levelname)s - %(message)s'
+    logging.basicConfig(format=logFormatter, level=logging.INFO)
+    logger = logging.getLogger(__name__)
     
-    process_file(xls_name)
+    process_file(file_path)
 
