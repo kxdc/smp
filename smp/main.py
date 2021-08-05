76a7a55d9c300370972414e395e83bc06936ba9d
kxdc <kvuexlu@gmail.com>
2021-08-06 05:58:55 +0800

    Fix some code linter issues

--- smp/main.py
+++ smp/main.py
@@ -10,64 +10,59 @@ Content_Results = List[Tuple[int, List[Oneline_Results]]]
 
 
 class SimpleMailHelper:
-    target_word_list=[]
-    content=[]
+    target_word_list = []
+    content = []
 
     def set_target_word_list(self, word_list: List[str]) -> None:
 
-        self.target_words_list=word_list
+        self.target_words_list = word_list
 
     def exists_file(self, filename: str) -> bool:
-    
+
         if not os.path.exists(filename):
             logger.critical('No such file "%s"', filename)
             return False
         else:
             logger.info('Processing file "%s"', filename)
             return True
-    
-    
+
     def get_lines(self, filename: str) -> str:
-    
+
         with open(filename, "rt") as file:
             lines = file.read()
-    
+
         return lines
-    
-    
+
     def process_file(self, filename: str) -> None:
-    
+
         if self.exists_file(filename):
             for oneline in self.get_lines(filename).split("."):
                 self.content.append(oneline.lstrip())
-    
+
     def search_substr_in_line(self, line: str, substr: str) -> Oneline_Results:
-    
+
         return (substr, [string.start() for string in re.finditer(substr, line)])
-    
-    
+
     def search_word_in_line(self, line: str, word: str) -> Oneline_Results:
-    
+
         return (word, [idx for idx, wd in enumerate(line.split()) if wd == word])
-    
-    
+
     def search_words_in_content(self) -> Content_Results:
-    
+
         results = []
         line_index = -1
-    
+
         for oneline in self.content:
             line_index += 1
             oneline_result = []
-            for target_word in self.target_word_list:
-                oneline_result.append(self.search_word_in_line(oneline, target_word))
+            for word in self.target_word_list:
+                oneline_result.append(self.search_word_in_line(oneline, word))
             results.append((line_index, oneline_result))
-    
+
         return results
-    
-    
+
     def process_words_to_remove(self, results: Content_Results) -> None:
-    
+
         for oneline_result in results:
             found = False
             hint_line = ""
@@ -76,7 +71,7 @@ class SimpleMailHelper:
                     found = True
                     position_hit = ",".join(str(c) for c in word_hit[1])
                     hint_line = word_hit[0] + ":" + position_hit
-    
+
             print(self.content[oneline_result[0]])
             if found:
                 print(hint_line, end="")
