98d14f1860ebba9a4c42ca1dffce05ac5e572ab3
kxdc <kvuexlu@gmail.com>
2021-08-16 20:08:04 +0800

    Add first unit test of class SimpleMailHelper

--- smp/tests/test_smp.py
+++ smp/tests/test_smp.py
@@ -1,5 +1,12 @@
 from smp import __version__
+from main import SimpleMailHelper
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def test_exists_file():
+    helper = SimpleMailHelper()
+    assert helper.exists_file("../sample.txt") == True
+    assert helper.exists_file("../../not_here.txt") == False
